5aaf76d4f0e8e5b46eebe40c213b1b45418e5a74
prashant-ux <prashant.tiwari_ec16@gla.ac.in>
2021-07-23 12:55:32 +0530

    add reverse an array code

---
+++ reverseAnArray.js
@@ -0,0 +1,18 @@
+var n = 7545;
+
+function reverseAnArray (integer)
+{
+  let o = integer.toString().split("");
+  console.log(o);
+  let c = [] ;
+  for(let i = o.length-1 ; i >= 0; i--)
+  {
+    c.push(o[i]);
+  console.log(c);
+}
+let x = c.join("");
+console.log(x); 
+let b = parseInt(x);
+console.log(b)
+}
+reverseAnArray(n);
