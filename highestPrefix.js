4f98b206d5b1b85ea026b6ead084e33d7773f20c
prashant-ux <prashant.tiwari_ec16@gla.ac.in>
2021-07-23 13:22:03 +0530

    updated program of HighestPrefix

--- highestPrefix.js
+++ highestPrefix.js
@@ -1,15 +1,30 @@
-var a = ["flower","flu","flow","flush"]
+var a = ["flower", "flu", "flow", "flush"];
 var b = "";
-for(let i = 0; i < a.length; i++)
-{
-    for(let j = i+1; j < a.length; j++)
-    {
-        if(a[i][i] !== a[j][i])
-        {
-            b = a[i][i],a[j][i];
-        }
-         console.log(a[i][i],a[j][i]);
-        
+
+function checkPrefix(str1, str2) {
+  let length1 = str1.length;
+  let length2 = str2.length;
+  let i = 0;
+  let j = 0;
+  result = "";
+  while (i <= length1 - 1 && j <= length2 - 1) {
+    if (str1[i] !== str2[j]) {
+      break;
     }
-        
-    }
+    result += str1[i];
+    j += 1;
+    i += 1;
+  }
+  return result;
+}
+
+function longestCommonPrefix(a) {
+  let prefix = a[0];
+  let result = ""
+  for (let i = 1; i < a.length; i++) {
+    result = checkPrefix(prefix, a[i]);
+  }
+  console.log(result)
+}
+
+longestCommonPrefix(a);
